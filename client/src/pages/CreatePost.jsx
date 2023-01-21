7ab9236c2fdff526c213b78b4d4e2335f4332eb0
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2023-01-21 17:42:23 -0500

    Prompt and image preview

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -55,6 +55,21 @@ const CreatePost = () => {
             handleSurpriseMe={handleSurpriseMe}
           />
         </div>
+        <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 justify-center items-center flex'>
+          {form.photo ? (
+            <img
+            src={form.photo}
+            alt={form.prompt}
+            className="w-full h-full object-contain"
+            />
+          ) : (
+            <img 
+              src={preview}
+              alt="preview"
+              className='w-9/12 h-9/12 object-contain opacity-40'
+            />
+          )}
+      </div>
       </form>
     </section>
   )
