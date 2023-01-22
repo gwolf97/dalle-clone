ae67689906d8870c4b39bb5d67aff76c4fde167f
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2023-01-22 15:31:58 -0500

    Generate image functionality

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -10,6 +10,7 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 })
 
+
 const openai = new OpenAIApi(configuration)
 
 router.route("/").get((req,res) => {
@@ -20,6 +21,7 @@ router.route("/").post(async (req,res) => {
     try {
         const {prompt} = req.body
 
+        
         const aiResponse = await openai.createImage({
             prompt,
             n: 1,
@@ -29,7 +31,7 @@ router.route("/").post(async (req,res) => {
 
         const image = aiResponse.data.data[0].b64_json
 
-        rest.status(200).json({photo: image})
+        res.status(200).json({photo: image})
     } catch (error) {
         res.status(500).send(error?.response.data.error.message)
     }
